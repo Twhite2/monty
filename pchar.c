fea44d26b3821fdc7c6e1445ffc29ea2ea5d27c3
Twhite2 <emmanuelmieye@gmail.com>
2022-07-14 12:12:19 -0700

    task update

--- pchar.c
+++ pchar.c
@@ -19,7 +19,7 @@ free(bus.content);
 free_stack(*head);
 exit(EXIT_FAILURE);
 }
-if (h->n 127 || h->n < 0)
+if (h->n > 127 || h->n < 0)
 {
 fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
 fclose(bus.file);
